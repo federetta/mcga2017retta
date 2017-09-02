e644d44c953f3bef5a65966140db0fe4f5a46116
Federico Retta <federico.retta@gmail.com>
2017-09-02 15:23:17 -0300

    Initial Commit

---
+++ SolutionsLeatherGoods/Presentation/ASF.UI.Process/CategoryProcess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using ASF.Entities;
+using ASF.Services.Contracts;
+using ASF.UI.Process;
+
+namespace ASF.UI.Process
+{
+    public class CategoryProcess : ProcessComponent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Category> SelectList()
+        {
+            var response = HttpGet<AllResponse>("rest/Category/All", new Dictionary<string, object>(), MediaType.Json);
+            return response.Result;
+        }
+    }
+}
