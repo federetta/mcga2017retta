83e729dcd4b38f3868b56d4c3ef1fc1f4ebfd796
Federico Retta <federico.retta@gmail.com>
2017-09-04 20:12:48 -0300

    CategoriesIndex

---
+++ SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Areas/Categories/CategoriesAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ASF.UI.WbSite.Areas.Categories
+{
+    public class CategoriesAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Categories";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Categories_default",
+                "Categories/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Areas/Categories/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASF.UI.Process;
+
+namespace ASF.UI.WbSite.Areas.Categories.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Categories/Category
+        public ActionResult Index()
+        {
+            var cp = new CategoryProcess();
+            var lista = cp.SelectList();
+            return View(lista);
+        }
+    }
+}
